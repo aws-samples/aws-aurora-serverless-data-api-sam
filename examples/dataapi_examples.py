5cc604241bd8db3a565a79fd41b1d24d81458b6c
Marcilio Mendonca <marcilio@amazon.com>
2019-06-04 08:53:05 -0400

    Removing boto3 dependency

--- examples/dataapi_examples.py
+++ examples/dataapi_examples.py
@@ -1,22 +1,29 @@
 import boto3
 import json
 import os
+import time
 
-def get_cfn_output(key, outputs):
-    result = [ v['OutputValue'] for v in outputs if v['OutputKey'] == key ]
-    return result[0] if len(result) > 0 else ''
+# Update these 3 parameters for your environment
+database_name = 'ec2_inventory_db'
+db_cluster_arn = 'arn:aws:rds:us-east-1:665243897136:cluster:dev-aurora-ec2-inventory-cluster'
+db_credentials_secrets_store_arn = 'arn:aws:secretsmanager:us-east-1:665243897136:secret:dev-AuroraUserSecret-DhpkOI'
 
-# Retrieve required parameters from RDS stack exported output values
-rds_stack_name = os.getenv('rds_stack_name')
-cloudformation = boto3.resource('cloudformation')
-stack = cloudformation.Stack(rds_stack_name)
-database_name = get_cfn_output('DatabaseName', stack.outputs)
-db_cluster_arn = get_cfn_output('DatabaseClusterArn', stack.outputs)
-db_credentials_secrets_store_arn = get_cfn_output('DatabaseSecretArn', stack.outputs)
-print(f'Database info: [name={database_name}, cluster arn={db_cluster_arn}, secrets arn={db_credentials_secrets_store_arn}]')
-
+# This is the Data API client that will be used in our examples below
 rds_client = boto3.client('rds-data')
 
+def timeit(f):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = f(*args, **kw)
+        te = time.time()
+        print(f'Function: {f.__name__}')
+        print(f'*  args: {args}')
+        print(f'*  kw: {kw}')
+        print(f'*  execution time: {(te-ts)*1000:8.2f} ms')
+        return result
+    return timed
+
+@timeit
 def execute_statement(sql, sql_parameters=[]):
     response = rds_client.execute_statement(
         secretArn=db_credentials_secrets_store_arn,
@@ -27,12 +34,9 @@ def execute_statement(sql, sql_parameters=[]):
     )
     return response
 
-execute_statement('delete from package')
-execute_statement('insert into package (package_name, package_version) values ("package100", "version-1")')
-execute_statement('insert into package (package_name, package_version) values ("package101", "version-1")')
-
 # Create the DB Schema (here just table package)
-def example0():
+@timeit
+def example_create_table():
     print('===== Example - create schema from DDL file =====')
     execute_statement(f'create database if not exists {database_name}')
     table_ddl_script_file = 'table_package.txt'
@@ -40,9 +44,15 @@ def example0():
     with open(table_ddl_script_file, 'r') as ddl_script:
         ddl_script_content=ddl_script.read()
         execute_statement(ddl_script_content)
+    # add some data
+    execute_statement('delete from package')
+    for i in range(100,110):
+        execute_statement(f'insert into package (package_name, package_version) values ("package-{i}", "version-1")')
+        execute_statement(f'insert into package (package_name, package_version) values ("package-{i}", "version-2")')
 
 # Simple select example with no parameters
-def example1():
+@timeit
+def example_simple_select():
     print('===== Example - Simple select statement =====')
     def execute_statement(sql):
         response = rds_client.execute_statement(
@@ -54,21 +64,23 @@ def example1():
         return response
 
     response = execute_statement(f'select * from package')
-    print(response['records'])
+    print(f'response: {response}')
 
 # Simple select example with parameters
 # Talk about parameters as a means to prevent SQL injections
-def example2():
+@timeit
+def example_select_with_parameters():
     print('===== Example - select with parameters =====')
     sql = 'select * from package where package_name=:package_name'
-    package_name = 'package100'
+    package_name = 'package-100'
     sql_parameters = [{'name':'package_name', 'value':{'stringValue': f'{package_name}'}}]
     response = execute_statement(sql, sql_parameters)
-    print(response['records'])
+    print(f'response: {response}')
 
 # Fetch results
 # Order of parameters on select is relevant (eg, package_name, package_version)
-def example3():
+@timeit
+def example_fetch_select_results():
     print('===== Example - fetch select results =====')
 
     def build_object_from_response(response):
@@ -85,29 +97,31 @@ def example3():
     sql = 'select package_name, package_version from package'
     response = execute_statement(sql)
     result = build_object_from_response(response)
-    print(result)
+    print(f'result: {result}')
 
 # Simple insert example
-def example4():
+@timeit
+def example_simple_insert_with_parameters():
     print('===== Example - simple insert with parameters =====')
     sql = 'insert into package (package_name, package_version) values (:package_name, :package_version)'
     sql_parameters = [
-        {'name':'package_name', 'value':{'stringValue': 'package-2'}},
+        {'name':'package_name', 'value':{'stringValue': 'package-1'}},
         {'name':'package_version', 'value':{'stringValue': 'version-1'}}
     ]
     response = execute_statement(sql, sql_parameters)
     print(f'Number of records updated: {response["numberOfRecordsUpdated"]}')
 
 # Handling exceptions
-def example5():
-    print('===== Example - handling exceptions =====')
+@timeit
+def example_exception_handling():
+    print('===== Example - handling exceptions - Duplicate Primary Key =====')
     class DataAccessLayerException(Exception):
         pass
     def add_package():
         try:
             sql = 'insert into package (package_name, package_version) values (:package_name, :package_version)'
             sql_parameters = [
-                {'name':'package_name', 'value':{'stringValue': 'package-2'}},
+                {'name':'package_name', 'value':{'stringValue': 'package-1'}},
                 {'name':'package_version', 'value':{'stringValue': 'version-1'}}
             ]
             response = execute_statement(sql, sql_parameters)
@@ -120,6 +134,7 @@ def example5():
         print(e)
 
 # Introduce batch inserts
+@timeit
 def batch_execute_statement(sql, sql_parameter_sets):
     response = rds_client.batch_execute_statement(
         secretArn=db_credentials_secrets_store_arn,
@@ -132,8 +147,9 @@ def batch_execute_statement(sql, sql_parameter_sets):
 
 # Batch insert example
 # Ask Data API what's the max batch size!
-def example6():
-    print('===== Example - batch SQL statements =====')
+@timeit
+def example_batch_insert():
+    print('===== Example - batch SQL Insert statements =====')
     sql = 'insert into package (package_name, package_version) values (:package_name, :package_version)'
     sql_parameter_sets = []
     for i in range(10,20):
@@ -146,7 +162,8 @@ def example6():
     print(f'Number of records updated: {len(response["updateResults"])}')
 
 # Transactions (commit and rollback)
-def example7():
+@timeit
+def example_transactions(package_start_idx, package_end_idx):
     print('===== Example - transactions with commit and rollback =====')
     # begin transaction
     transaction = rds_client.begin_transaction(
@@ -156,14 +173,15 @@ def example7():
     try:
         sql = 'insert into package (package_name, package_version) values (:package_name, :package_version)'
         sql_parameter_sets = []
-        for i in range(30,40):
+        for i in range(package_start_idx,package_end_idx):
             entry = [
-                    {'name':'package_name', 'value':{'stringValue': f'package{i}'}},
+                    {'name':'package_name', 'value':{'stringValue': f'package-{i}'}},
                     {'name':'package_version', 'value':{'stringValue': 'version-1'}}
             ]
             sql_parameter_sets.append(entry)
         response = batch_execute_statement(sql, sql_parameter_sets)
-    except Exception:
+    except Exception as e:
+        print(f'Error: {e}')
         transaction_response = rds_client.rollback_transaction(
             secretArn=db_credentials_secrets_store_arn,
             resourceArn=db_cluster_arn,
@@ -176,13 +194,32 @@ def example7():
         print(f'Number of records updated: {len(response["updateResults"])}')
     print(f'Transaction Status: {transaction_response["transactionStatus"]}')
 
-# Running our examples in sequence
-example0()
-example1()
-example2()
-example3()
-example4()
-example5()
-example6()
-example7()
 
+# Running our examples in order
+example_create_table()
+print('\n')
+
+example_simple_select()
+print('\n')
+
+example_select_with_parameters()
+print('\n')
+
+example_fetch_select_results()
+print('\n')
+
+example_simple_insert_with_parameters()
+print('\n')
+
+example_exception_handling()
+print('\n')
+
+example_batch_insert()
+print('\n')
+
+# key 100 is a duplicate - transaction will rollback
+example_transactions(91,101)
+print('\n')
+
+# transaction will be committed successfully
+example_transactions(1000,1020)
